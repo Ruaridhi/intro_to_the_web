d0602154341833e50ea5437c0a11ebd24d9b934a
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-7.local>
2018-09-18 17:45:54 +0100

    add 2 players

--- battle/spec/features/test_infrastructure_spec.rb
+++ battle/spec/features/test_infrastructure_spec.rb
@@ -8,8 +8,9 @@ feature 'visit_homepage', type: :feature do
 
   scenario 'users put in name' do
     visit '/'
-    fill_in 'name', with: 'Rory'
+    fill_in 'name1', with: 'Rory'
+    fill_in 'name2', with: 'Seb'
     click_button 'Submit'
-    expect(page).to have_content('Rory')
+    expect(page).to have_content('Rory vs Seb')
   end
 end
