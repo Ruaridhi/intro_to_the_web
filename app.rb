fcc7161b02d2dabe7d4bc6b8ee9ef85c8fa7e07f
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-7.local>
2018-09-18 13:19:34 +0100

    using erb file

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require 'sinatra'
 
-get '/' do
-  "Hello!"
-  "kuhdauduwed"
-end
-
-get '/hello/:name' do
-  # matches "GET /hello/foo" and "GET /hello/bar"
-  # params['name'] is 'foo' or 'bar'
-  "Hello #{params['name']}!"
-end
-
-get '/secret'do
-  "Hello, again..."
-end
+# get '/' do
+#   "Hello!"
+#   "kuhdauduwed"
+# end
+#
+# get '/hello/:name' do
+#   # matches "GET /hello/foo" and "GET /hello/bar"
+#   # params['name'] is 'foo' or 'bar'
+#   "Hello #{params['name']}!"
+# end
+#
+# get '/secret'do
+#   "Hello, again..."
+# end
 
 get '/cat'do
   erb(:index)
