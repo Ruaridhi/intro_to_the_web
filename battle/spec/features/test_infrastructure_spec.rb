226aae61be62a2ded0aafe29324f8eaf01a9deb8
Chloe Mingay <chloemingay@MacBook-Pro-3.local>
2018-09-19 15:55:21 +0100

    Can see hit points

--- battle/spec/features/test_infrastructure_spec.rb
+++ battle/spec/features/test_infrastructure_spec.rb
@@ -1,17 +1,20 @@
 require './app'
 
 feature 'visit_homepage', type: :feature do
-  # scenario 'hompage says testing infrastructure working!' do
-  #   visit '/'
-  #   expect(page).to have_content('Testing infrastructure working!')
-  # end
-
-
   scenario 'prints page title and users put in names' do
     visit '/'
     fill_in 'name1', with: 'Rory'
     fill_in 'name2', with: 'Seb'
     click_button 'Submit'
     expect(page).to have_content('Rory vs Seb')
+  end
+
+  scenario 'Hit points revealed' do
+    visit '/'
+    fill_in 'name1', with: 'Rory'
+    fill_in 'name2', with: 'Seb'
+    click_button 'Submit'
+    expect(page).to have_content('Seb\'s Hit Points: 100')
+    save_and_open_page(path = nil)
   end
 end
